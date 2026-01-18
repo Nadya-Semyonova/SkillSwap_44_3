37ce0142cdde5580a9fe16f6beb334483f680edf
RaiHammer <arta-daniil@ya.ru>
2026-01-18 18:00:25 +0300

    footerStatic

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import { Footer } from '../widgets/Footer/index';
 
 export function Layout() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 }
