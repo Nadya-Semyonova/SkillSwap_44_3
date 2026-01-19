53ffa63502366fc3ea78bda6fe5de2815eb488ba
RaiHammer <arta-daniil@ya.ru>
2026-01-19 19:34:41 +0300

    modalСomponents

--- src/shared/ui/modal/types.ts
+++ src/shared/ui/modal/types.ts
@@ -5,3 +5,27 @@ export interface ModalProps {
   onClose: () => void;
   children: ReactNode;
 }
+
+export interface ConfirmModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+  onConfirm: () => void;
+}
+
+export interface InfoModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+  onConfirm: () => void;
+}
+
+export interface NoticeModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+  onConfirm: () => void;
+}
