1ec030b742847fc70f62b424b6e29631fbc6e929
Asumodeusu <pop.illya@yandex.ru>
2026-01-20 20:49:34 +0500

    Update AppRoute.tsx

--- src/app/AppRoute.tsx
+++ src/app/AppRoute.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from '../layouts/layout';
-import { ROUTES } from '../shared/lib/constants/routes';
+import { Layout } from '@layouts/layout';
+import { ROUTES } from '@shared/lib/constants/routes';
 import { HomePage } from '../pages/HomePage/HomePage';
-import LoginPage from '../pages/LoginPage/LoginPage';
-import RegisterPage from '../pages/RegisterPage/RegisterPage';
+import { LoginPage } from '../pages/LoginPage/LoginPage';
+import { RegisterPage } from '../pages/RegisterPage/RegisterPage';
 
 export default function AppRoute() {
   return (
