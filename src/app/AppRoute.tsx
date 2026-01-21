a71bd0432b6b44ec96e6611f40cde18c1b644fe8
Katniss-kk <katniss1111@yandex.ru>
2026-01-21 05:30:34 +0500

    Update AppRoute.tsx

--- src/app/AppRoute.tsx
+++ src/app/AppRoute.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from '@layouts/layout';
 import { ROUTES } from '@shared/lib/constants/routes';
 import { HomePage } from '../pages/HomePage/HomePage';
-import RegistrationStep3 from '../features/auth/registration/ui/RegistrationStep3/RegistrationStep3';
 
 export default function AppRoute() {
   return (
@@ -15,7 +14,6 @@ export default function AppRoute() {
       <Routes>
         <Route element={<Layout />}>
           <Route path={ROUTES.HOME} element={<HomePage />} />
-          <Route path="/step3" element={<RegistrationStep3 />} />
         </Route>
       </Routes>
     </BrowserRouter>
