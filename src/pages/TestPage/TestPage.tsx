d62bd674a0f3d43f353ddebe4f8e7abc5eeab3d4
Asumodeusu <pop.illya@yandex.ru>
2026-01-22 17:11:01 +0500

    cardPhoto

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,11 +1,13 @@
-import { RegistrationStep3 } from '@features/auth/registration/ui';
-import HeaderRegister from '@features/auth/registration/ui/HeaderRegistration/headerRegistration';
+// import SkillsSelector from '@/widgets/SkillsSelector/SkillsSelector';
+import { RegistrationStep3 } from '@/features/auth/registration/ui';
+import HeaderRegister from '@/features/auth/registration/ui/HeaderRegistration/headerRegistration';
 
 export function TestPage() {
   return (
     <>
       <HeaderRegister />
       <RegistrationStep3 />
+      {/* <SkillsSelector /> */}
     </>
   );
 }
