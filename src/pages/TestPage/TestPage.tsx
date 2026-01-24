18a71aedb032368cd7b8957e1224e53688a489af
KseniiaArk <mogilevicsena@gmail.com>
2026-01-24 15:43:35 +0300

    fix: созданы переиспользуемые компоненты в CardPhoto

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,22 +1,11 @@
 import HeaderRegister from '@features/auth/registration/ui/HeaderRegistration/headerRegistration';
 import { RegistrationStep3 } from '@features/auth/registration/ui';
-import CardPhoto from '@/widgets/CardPhoto';
-import { MOCK_FOR_CARDPHOTO_COMPONENT } from '@/widgets/CardPhoto/Mock';
 
 export function TestPage() {
   return (
     <>
       <HeaderRegister />
       <RegistrationStep3 />
-      <CardPhoto
-        user={MOCK_FOR_CARDPHOTO_COMPONENT}
-        onLike={() => undefined}
-        onShare={() => undefined}
-        onMoreDetails={() => undefined}
-        onExchange={() => undefined}
-        isOwner={false}
-        requestSent={false}
-      />
     </>
   );
 }
