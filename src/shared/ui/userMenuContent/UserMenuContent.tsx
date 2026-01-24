d06ba6a68ca7a136871d15dacffe6f6931299940
Polina Fan <fanjeewon@yandex.ru>
2026-01-25 01:18:19 +0900

    Кнопка выхода из профиля

--- src/shared/ui/userMenuContent/UserMenuContent.tsx
+++ src/shared/ui/userMenuContent/UserMenuContent.tsx
@@ -1,5 +1,7 @@
 import Logout from '@/shared/assets/images/IconsSvg/Logout';
 import styles from './userMenuContent.module.css';
+import { useDispatch } from '@/store/store';
+import { logout } from '@/store/slices/authSlice/authSlice';
 
 type Props = {
   onProfileClick: () => void;
@@ -7,13 +9,22 @@ type Props = {
 };
 
 export default function UserMenuContent({ onProfileClick, onLogoutClick }: Props) {
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.dropdown}>
       <button type="button" className={styles.dropdownItem} onClick={onProfileClick}>
         Личный кабинет
       </button>
 
-      <button type="button" className={styles.dropdownItem} onClick={onLogoutClick}>
+      <button
+        type="button"
+        className={styles.dropdownItem}
+        onClick={() => {
+          dispatch(logout());
+          onLogoutClick();
+        }}
+      >
         Выйти из аккаунта
         <Logout />
       </button>
