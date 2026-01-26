069572fcb0436e10650cd0b4d5e6b451caa916e2
Asumodeusu <pop.illya@yandex.ru>
2026-01-26 15:28:33 +0500

    Update authSlice.ts

--- src/store/slices/authSlice/authSlice.ts
+++ src/store/slices/authSlice/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { setTokenCookie, getTokenCookie, removeTokenCookie } from '@/shared/lib/cookies';
+import { setTokenCookie, getTokenCookie, removeTokenCookie } from '@shared/lib/cookies';
 import {
   setUserToLocalStorage,
   getUserFromLocalStorage,
